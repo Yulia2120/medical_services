9d23d4ffdf55e78c752e93fe216b925ecf5da941
Yuliya <iksulia1@gmail.com>
2024-04-18 23:10:05 +0300

    edit main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // mobile nav
-const mobileNav = document.querySelector(".mnav");
-const closeBtn = document.querySelector(".mnav__close-btn");
-const closeBtnIcn = document.querySelector(".mnav__close-btn-icon");
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcn = document.querySelector('.mnav__close-btn-icon');
 
-const navOpenedClass = "left-0";
-const navClosedClass = "-left-[300px]";
-const arrowLeftClass = "ri-arrow-left-s-line";
-const arrowRightClass = "ri-arrow-right-s-line";
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener('click', () => {
   mobileNav.classList.toggle(navOpenedClass);
   mobileNav.classList.toggle(navClosedClass);
 
@@ -18,111 +18,111 @@ closeBtn.addEventListener("click", () => {
 
 //swiper
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
 });
 
 //faq
-const faqItems = document.querySelectorAll(".faq__item");
+const faqItems = document.querySelectorAll('.faq__item');
 
-faqItems.forEach((item) => {
-  const faqBtn = item.querySelector(".faq__btn");
+faqItems.forEach(item => {
+  const faqBtn = item.querySelector('.faq__btn');
 
-  item.addEventListener("click", () => {
-    const isOpen = item.classList.toggle("open");
-    const iconClass = isOpen ? "ri-subtract-fill" : "ri-add-fill";
-    const iconElement = faqBtn.querySelector("i");
+  item.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('open');
+    const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
+    const iconElement = faqBtn.querySelector('i');
     iconElement.classList = `${iconClass} text-2xl`;
   });
 });
 
 //scroll reveal animations
 const sr = ScrollReveal({
-  origin: "bottom",
-  distance: "60px",
-  duration: 3000,
+  origin: 'bottom',
+  distance: '60px',
+  duration: 2000,
   delay: 600,
   // reset: true, //resets animation
 });
 
 //hero
-sr.reveal(".hero__text", { origin: "top" });
-sr.reveal(".hero__img");
+sr.reveal('.hero__text', { origin: 'top' });
+sr.reveal('.hero__img');
 
 //stats
-sr.reveal(".stats__item", {
+sr.reveal('.stats__item', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "top",
+  origin: 'top',
 });
 
 //services
-sr.reveal(".services");
-sr.reveal(".services__top");
-sr.reveal(".services__item", {
+sr.reveal('.services');
+sr.reveal('.services__top');
+sr.reveal('.services__item', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "bottom",
+  origin: 'bottom',
 });
 
 //appointment
-sr.reveal(".appointment__title");
-sr.reveal(".appointment__form");
+sr.reveal('.appointment__title');
+sr.reveal('.appointment__form');
 
 //testimonials
-sr.reveal(".testimonial");
-sr.reveal(".testimonial__container");
+sr.reveal('.testimonial');
+sr.reveal('.testimonial__container');
 
 //team
-sr.reveal(".team__title");
-sr.reveal(".team__slider");
+sr.reveal('.team__title');
+sr.reveal('.team__slider');
 
 //faq
-sr.reveal(".faq__title");
-sr.reveal(".faq__item", {
+sr.reveal('.faq__title');
+sr.reveal('.faq__item', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "bottom",
+  origin: 'bottom',
 });
 
 //departments
-sr.reveal(".departments__bg");
-sr.reveal(".departments__container");
+sr.reveal('.departments__bg');
+sr.reveal('.departments__container');
 
 //blog
-sr.reveal(".blog__title");
-sr.reveal(".blog__post", {
+sr.reveal('.blog__title');
+sr.reveal('.blog__post', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "bottom",
+  origin: 'bottom',
 });
 
 //brands
-sr.reveal(".brands__logo", {
+sr.reveal('.brands__logo', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "bottom",
+  origin: 'bottom',
 });
 
 //newsletter
-sr.reveal(".newsletter");
-sr.reveal(".newsletter__container");
+sr.reveal('.newsletter');
+sr.reveal('.newsletter__container');
 
 //footer
-sr.reveal(".footer__item", {
+sr.reveal('.footer__item', {
   delay: 600,
-  distance: "100px",
+  distance: '100px',
   interval: 100,
-  origin: "bottom",
+  origin: 'bottom',
 });
